833b92f88b0beceddb803073298cf328a93aa688
Pedro Lucas <pedroplpa@hotmail.com>
2017-07-04 20:44:23 -0300

    Added configuration file and parameter history

--- core.py
+++ core.py
@@ -1,12 +1,48 @@
 import importlib
-name = raw_input("Choose the module: ")
+import json
+import os.path
 
-module_test = importlib.import_module(name)
+#Initial configuration
+with open("config.json") as config_file:
+	config = json.load(config_file)
+while True:
+	#Showing Modules
+	module_names = config.keys()
+	print("\nChoose one of the modules: ")
+	for name in module_names:
+		print(" -> " + name)
+	print(" -> exit")
 
-querie = module_test.BEGIN()
+	#Choosing module
+	print
+	choosen_module = raw_input("Module: ")
+	
+	#If exit is choosen
+	if choosen_module == "exit":
+		break
+	
+	#Module choosen
+	if choosen_module in module_names:
+		module = importlib.import_module(config[choosen_module])
+		
+		if os.path.isfile(choosen_module + ".json"):
+			answer = raw_input("Use the last used parameters? [yes/no] ")
+			if answer == "yes":
+				module.RUN(choosen_module)
+				continue
 
-dic = {}
-for field in querie:
-	dic[field] = raw_input(field + ": ")
+		querie = module.BEGIN()
 
-module_test.RUN(dic)
+		dic = {}
+		for field in querie:
+			dic[field] = raw_input(field + ": ")
+		with open(choosen_module + ".json", "w") as parameters:
+			json.dump(dic, parameters)
+
+		module.RUN(choosen_module)
+
+	#Invalid Module
+	else:
+		print("Invalid module!!")
+print
+print("PentIME")
--- module_test.py
+++ module_test.py
@@ -1,7 +1,11 @@
+import json
+
 def BEGIN():
 	res = ["Name","Age"]
 	return res
 
 
-def RUN(dic):
-	print dic.keys()
+def RUN(file_name):
+	with open(file_name + ".json", "r") as input_data:
+		parameters = json.load(input_data)
+	print("\nHello, my name is " + parameters["Name"] + " and I'm " + parameters["Age"] + " years old.")
