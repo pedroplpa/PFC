9f68ea159a0fa4265b0ef3aec109a97cdf2e3260
bmatissek <bruno.matissek@gmail.com>
2017-07-05 14:32:23 -0300

    Primero commit do desenvolvimento do modulo de SQL Injection, com o parser de HTML

---
+++ sql/parser.py
@@ -0,0 +1,28 @@
+from html.parser import HTMLParser
+import requests
+
+class HTMLTargetParser(HTMLParser):
+	inForm = False
+	targetTagList = ['select','input','textarea']
+
+	def handle_starttag(self, tag, attrs):
+		if tag in self.targetTagList:
+			print ('Tag detected: ' + tag + ' with following attributes')
+			print (attrs)
+		if tag == 'form':
+			print ('Form detected with following attributes:')
+			print (attrs)
+			self.inForm = True
+	def handle_endtag(self, tag):
+		if tag == 'form':
+			print ('End of form')
+			self.inForm = False
+
+url = 'http://ctf.imesec.org/web-4444'
+data_values={'userName':'cebola ',
+	'userPassword':'batata'}
+r = requests.post(url,data = data_list)
+targetParser = HTMLTargetParser()
+targetParser.feed(r.text)
+
+print (r.text)
