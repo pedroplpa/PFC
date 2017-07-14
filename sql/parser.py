8c55653c61f08541e64bb9cf7d71f1f80d1a4f4a
bmatissek <bruno.matissek@gmail.com>
2017-07-14 11:33:53 -0300

    Continuacao do Desenvolvimento do Parser HTML

--- sql/parser.py
+++ sql/parser.py
@@ -5,45 +5,58 @@ import string
 class HTMLForm:
 	formType = "GET"
 	elementList = []
+
 	def __init__(self,formType):
 		self.formType = formType
-	def addElement(self,tag,attrs):
-		self.elementList.append([tag,attrs])
+
+	def addElement(self,tag,elementName,elementType):
+		self.elementList.append([tag,elementName,elementType])
+
+	def processElement(self,tag,attrs):
+		elementName=""
+		elementType="input"
+		for attr in attrs:
+			if (str.upper(attr[0]) == "NAME"):
+				elementName = attr[1]
+			if (str.upper(attr[0]) == "TYPE"):
+				elementType = attr[1]
+		#Ensure that the SUBMIT field will not be included in the target element list
+		if (str.upper(elementType) != "SUBMIT"):
+			self.addElement(tag,elementName,elementType)
 
 class HTMLTargetParser(HTMLParser):
-	inForm = False
+	#For now, let us treat only the input elements of our HTML document
 	#targetTagList = ['select','input','textarea','option']
 	targetTagList = ['input']
+	inForm = False
 	formList = []
 	formObject = None
 	def handle_starttag(self, tag, attrs):
 		if tag in self.targetTagList:
 			if self.inForm:
-				self.formObject.addElement(tag,attrs)
-			print ('Tag detected: ' + tag + ' with following attributes')
-			print (attrs)
+				self.formObject.processElement(tag,attrs)
 		if tag == 'form':
-			print ('Form detected with following attributes:')
-			print (attrs)
 			method="GET"
 			for attr in attrs:
 				if (str.upper(attr[0]) == "METHOD"):
 					method = attr[1]
-					print (method)
 			self.formObject = HTMLForm(method)
 			self.inForm = True
 	def handle_endtag(self, tag):
 		if tag == 'form':
-			print ('End of form')
 			self.formList.append(self.formObject)
 			self.formObject = None
 			self.inForm = False
 
+
 url = 'http://ctf.imesec.org/web-4444'
+
 #data_values={'userName':'cebola',
 #	'userPassword':'batata'}
 #r = requests.post(url,data = data_values)
+
 r = requests.get(url)
 targetParser = HTMLTargetParser()
 targetParser.feed(r.text)
+print (targetParser.formList[0].formType)
 print (str(targetParser.formList[0].elementList))
