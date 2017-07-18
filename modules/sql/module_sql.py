0c85dce2482009f07003dd0d95f9640888b710bf
bmatissek <bruno.matissek@gmail.com>
2017-07-18 10:44:33 -0300

    Insercao do arquivo module_sql.py

---
+++ modules/sql/module_sql.py
@@ -0,0 +1,35 @@
+import json
+import requests
+from parser import HTMLForm
+from parser import HTMLTargetParser
+
+def BEGIN():
+	res = ["URL"]
+	return res
+
+def RUN(file_name):
+    url = 'http://ctf.imesec.org:4444'
+    try:
+        r = requests.get(url)
+        targetParser = HTMLTargetParser()
+        targetParser.feed(r.text)
+    except Exception:
+        print("[-] The URL did not respond to the request")
+        exit()
+
+    if not targetParser.formList:
+        print ("[-] The given URL had no forms for POST targeting")
+    else:
+        for form in targetParser.formList:
+            print (targetParser.formList[0].formType)
+            dataValues={}
+            for dataField in form.elementList:
+                dataValues.update({dataField[1]:'agora vai dar " ruim'})
+                print (dataValues)
+            try:
+                s = requests.post(url,data = dataValues)  
+                print (s.text)
+            except Exception:
+                print ("[-] An error occured while trying to POST data")
+
+RUN("mope")
