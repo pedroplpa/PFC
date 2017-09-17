0dda4f303fd57b6941b5dbd41759deec2b316171
Pedro Lucas <pedroplpa@hotmail.com>
2017-09-17 11:34:31 -0300

    created xss module

---
+++ modules/xss/module_xss.py
@@ -0,0 +1,122 @@
+from .userlib import *
+from collections import OrderedDict
+
+def BEGIN():
+	res = ["URL"]
+	return res
+
+def RUN(fileName):
+    #The report is an Ordered Dictionary for storing all relevant results for writing the JSON report
+    report = OrderedDict()
+    with open(fileName + ".json", "r") as inputData:
+        parameters = json.load(inputData)
+    url = parameters["URL"]
+    resultsFile = createResultsFile(fileName,url,report)
+    report["result"] = OrderedDict()
+
+    loginUrl ='http://192.168.0.43/bWAPP/login.php'
+    nonMaliciousValue='cebola'
+    attemptsFolder = os.path.dirname(__file__) + '/attempts/'
+    filePrefix = 'form-'
+    nonMaliciousResponseFileName = '-nonMaliciousAttempt'
+    errorBasedPayloadFileName =os.path.dirname(__file__) + '/payloads/error_based'
+    timeBasedPayloadFileName=os.path.dirname(__file__) + '/payloads/time_based'
+    
+    session = loginIntoBWAPPApplication(loginUrl)
+    print ("[+] Sending request for the URL web page")
+    targetParser = parseHtmlPage(url,session)
+    targetFormList = targetParser.formList
+    print ("[+] Parsing completed")
+
+    print ("[+] The following FORMS were found in the HTML document:")
+    reportFormList = []
+    for index,form in enumerate(targetParser.formList):
+        dataValues = createDictionary(form)
+        requestType = form.formType
+        print (" ID: #" + str(index) + " : " + requestType + " form with the following fields:")
+        print (dataValues)
+        reportFormList.append({str(index):str(dataValues)})
+    report["result"]["forms-detected"] = reportFormList
+
+    print ("Which of the forms will be tested? (space-separated ID list) or (all)")
+    options = input()
+
+    if str.lower(options) != 'all':
+        idList = [int(i) for i in options.split()]
+    else:
+        idList = list(range(0,len(targetFormList)))
+
+    report["result"]["forms-tested"] = str(idList) 
+    report["result"]["vulnerabilities"]=OrderedDict()
+    
+    for id in idList:
+        report["result"]["vulnerabilities"]["form-"+str(id)]=OrderedDict()
+        report["result"]["vulnerabilities"]["form-"+str(id)]["error-based"]=OrderedDict()
+        form = targetFormList[id]
+        dataValues = createDictionary(form,nonMaliciousValue)
+        filePath = attemptsFolder + filePrefix + str(id) + nonMaliciousResponseFileName
+        requestType = form.formType
+        print("[+] Sending non malicious request for FORM #" + str(id))
+        sendRequestAndSaveResponse(url,session,dataValues,requestType,filePath)
+
+        #Set our strategy for ERROR-BASED and test for every payload
+        detectedErrorBasedSQL = False
+        errorPayloadFile = open(errorBasedPayloadFileName,'r')
+        strategy = errorBasedSQLStrategy
+        print("[+] Testing form " + str(id) + " for error-based SQL injection detection")
+        for attempt,errorPayload in enumerate(errorPayloadFile.read().splitlines()): 
+            dataValues = createDictionary(form,errorPayload)
+            print("[+] Sending request for FORM #" + str(id) + ". PAYLOAD: " + errorPayload)
+            retVal = strategy(url,session,dataValues,requestType)
+            #If the first field of retVal is true, then there were vulnerabilities detected
+            if retVal[0]:
+                detectedErrorBasedSQL = True
+                print ("[!] Possible error-based vulnerability detected for form " 
+                        + str(id) + " using payload " + errorPayload)
+                report["result"]["vulnerabilities"]["form-"+str(id)]["error-based"]["payload "+errorPayload+" "]=retVal[1]
+                print ("Do you want to continue testing (y/n)?")
+                ans = input()
+                if (str.lower(ans) == "n"):
+                    break
+        errorPayloadFile.close()
+
+        #If an possible vulnerability was already detected, ask if the user wants to test for time-based
+        if detectedErrorBasedSQL:
+            print("[!] For FORM #" +str(id)+" there was an possible error-based SQL Injection " 
+                    + "vulnerability detected.")
+            print("Do you want to continue and test for time-based? (y/n)")
+            ans = input()
+            if (str.lower(ans) == "n"):
+                continue
+        
+        #Set our strategy for TIME-BASED and test for every payload
+        timeBasedPayloadFile = open(timeBasedPayloadFileName,'r')
+        report["result"]["vulnerabilities"]["form-"+str(id)]["time-based"]=OrderedDict()
+        strategy = timeBasedBlindSQLStrategy
+        print("[+] Testing form " + str(id) + " for time-based SQL injection detection")
+        for attempt,timePayload in enumerate(timeBasedPayloadFile.read().splitlines()): 
+            dataValues = createDictionary(form,timePayload)
+            print("[+] Sending request for FORM #" + str(id) + ". PAYLOAD: " + timePayload)
+            if strategy(url,session,dataValues,requestType):
+                print ("[!] Possible time-based vulnerability detected for form " 
+                        + str(id) + " using payload " + timePayload)
+                report["result"]["vulnerabilities"]["form-"+str(id)]["time-based"]["payload "+timePayload+" "]="success"
+                print ("Do you want to continue testing (y/n)?")
+                ans = input()
+                if (str.lower(ans) == "n"):
+                    break
+        timeBasedPayloadFile.close()
+
+    closeResultsFile(resultsFile,report)
+    return 0
+        #payloadFile = open(payloadFileName,'r')
+        #for attempt,payload in enumerate(payloadFile.read().splitlines()): 
+        #    dataValues = createDictionary(form,payload)
+        #    filePath = attemptsFolder + filePrefix+ str(id) + maliciousResponseFileName + str(attempt)
+        #    requestType=form.formType
+        #    print("[+] Sending request for FORM #" + str(id) + ". PAYLOAD: " + payload)
+        #    sendRequestAndSaveResponse(url,session,dataValues,requestType,filePath)
+        #payloadFile.close()
+        #print("[+] Finished all requests for FORM #" + str(id))
+
+#RUN("")
---
+++ modules/xss/userlib.py
@@ -0,0 +1,134 @@
+import json
+import requests
+import datetime
+import os
+from .parser import HTMLForm
+from .parser import HTMLTargetParser
+
+#Method for creating the results file for documenting all errors detected
+def createResultsFile(fileName,url,report):
+    date = datetime.datetime.now()
+    resultFileName = fileName+"_result"+".json"
+    resultsFile = open(resultFileName,'w')
+    report["title"] = {"date":date.strftime("%Y-%m-%d %H:%M:%S"),"url":url}
+    return resultsFile
+
+#Dumping all the report fields into the JSON file
+def closeResultsFile(resultsFile,report):
+    date = datetime.datetime.now()
+    report["result"]["finish-date"] = date.strftime("%Y-%m-%d %H:%M:%S")
+    json.dump(report,resultsFile)
+    resultsFile.close()
+
+#Method for logging into the application and creating a Session for accessing the 
+#different vulnerable pages in the bWAPP server for demonstration and testing
+def loginIntoBWAPPApplication(loginUrl,timeout = 3):
+    try:
+        session = requests.Session()
+        loginPayload = {'login':'bee','password':'bug','security_level':'0','form':'submit'}
+        login = session.post(loginUrl, data=loginPayload,timeout = timeout)
+    except requests.Timeout:
+        print ("[-] Login request timeout")
+        exit()
+    return session
+    
+#Method for parsing the web page and finding all HTML forms 
+def parseHtmlPage(url,session):
+    try:
+        r = session.get(url,timeout = 3)
+        print ("[+] Response received. Parsing HTML Document in order to detect FORMS")
+        targetParser = HTMLTargetParser()
+        targetParser.feed(r.text)
+        if not targetParser.formList:
+            print ("[-] The given URL had no forms for targetting")
+            exit()
+    except requests.Timeout:
+        print ("[-] Request for page timeout")
+        exit()
+    return targetParser
+
+#Method for creating the dictionary data structure according to the referred HTML Form
+#The dictonary structure will be used in the POST/GET requests
+def createDictionary(form,value = None):
+    dataValues={}
+    for dataField in form.elementList:
+        #The default dictionary is constructed using the inaltered value of the elements in the HTML
+        if value is None:
+            dataValues.update({dataField[1]:dataField[3]})
+        else:
+            #The action field of a button will not be processed by SQL
+            if str.lower(dataField[1]) == 'action':
+                dataValues.update({dataField[1]:dataField[3]})
+            else:
+                dataValues.update({dataField[1]:value})
+    return dataValues
+
+def sendRequestAndSaveResponse(url,session,dataValues,requestType,fileName,timeout = 3):
+    try:
+        if (requestType == 'POST'):
+            s = session.post(url, data = dataValues,timeout = timeout)  
+        if (requestType == 'GET'):
+            s = session.get(url, params = dataValues,timeout = timeout)
+    except requests.Timeout:
+        print ("[-] Request timeout")
+        return
+    os.makedirs(os.path.dirname(fileName),exist_ok=True)
+    f = open(fileName,'w')
+    f.write(s.text)
+    f.close()
+
+def errorBasedSQLStrategy (url,session, dataValues,requestType):
+    timeout = 4
+    result = False
+    messageList=[]
+    try:
+        if (requestType == 'POST'):
+            s = session.post(url, data = dataValues,timeout = timeout)  
+        if (requestType == 'GET'):
+            s = session.get(url, params = dataValues,timeout = timeout)
+    except requests.Timeout:
+        print ("[-] Request timeout")
+        return
+    with open(os.path.dirname(__file__) + "/sql_error_check","r") as checkFile:
+        checkLines = checkFile.readlines()
+        for line in checkLines:
+            if line.rstrip() in str.lower(s.text):
+                print ("[-] Detected \"" + line.rstrip() + "\" in the response, possible sql error-based vulnerability")
+                messageList.append(line.rstrip())
+                result = True
+    if not result:
+        print ("[-] No errors detected")
+    return result,messageList
+
+def timeBasedBlindSQLStrategy (url,session,dataValues,requestType):
+    #Defining the first timeout as 5 seconds
+    timeout = 5
+    retValue = False
+    try:
+        if (requestType == 'POST'):
+            s = session.post(url, data = dataValues,timeout = timeout)  
+        if (requestType == 'GET'):
+            s = session.get(url, params = dataValues,timeout = timeout)
+    except requests.Timeout:
+        #If the request timeouts, increase the timeout value to 10 seconds to ensure it is not a false positive
+        print ("[+] Possible time-based SQL injection vulnerability detected. Setting timeout for 10 seconds to reduce the chance of false-positive")
+        timeout = 10
+        print ("[+] Sending a new request to check if the server is OK.")
+        try:
+            if (requestType == 'POST'):
+                    s = session.post(url,timeout = timeout)  
+            if (requestType == 'GET'):
+                    s = session.get(url,timeout = timeout)
+            try:
+                print ("[+] Server is OK. Trying a new request to check ")
+                if (requestType == 'POST'):
+                    s = session.post(url, data = dataValues,timeout = timeout)  
+                if (requestType == 'GET'):
+                    s = session.get(url, params = dataValues,timeout = timeout)
+            except requests.Timeout:
+                print ("[+] Timeout reached, possible vulnerability detected. Yay!")
+                retValue = True
+                return retValue
+        except requests.Timeout:
+            print ("[-] Timeout reached, it seems that server crashed. Try again later.")
+            return retValue
