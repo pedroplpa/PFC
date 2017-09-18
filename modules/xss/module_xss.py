c2ed333717027c52418cff26790cf85922f1a004
Pedro Lucas <pedroplpa@hotmail.com>
2017-09-17 22:36:40 -0300

    implemented the xss reflected check with test xss payload

--- modules/xss/module_xss.py
+++ modules/xss/module_xss.py
@@ -2,8 +2,8 @@ from .userlib import *
 from collections import OrderedDict
 
 def BEGIN():
-	res = ["URL"]
-	return res
+    res = ["URL"]
+    return res
 
 def RUN(fileName):
     #The report is an Ordered Dictionary for storing all relevant results for writing the JSON report
@@ -13,23 +13,35 @@ def RUN(fileName):
     url = parameters["URL"]
     resultsFile = createResultsFile(fileName,url,report)
     report["result"] = OrderedDict()
-
-    loginUrl ='http://192.168.0.43/bWAPP/login.php'
+    ip=url.split('/')
+    loginUrl ='http://'+ ip[2] +'/bWAPP/login.php'
     nonMaliciousValue='cebola'
     attemptsFolder = os.path.dirname(__file__) + '/attempts/'
     filePrefix = 'form-'
     nonMaliciousResponseFileName = '-nonMaliciousAttempt'
-    errorBasedPayloadFileName =os.path.dirname(__file__) + '/payloads/error_based'
-    timeBasedPayloadFileName=os.path.dirname(__file__) + '/payloads/time_based'
-    
-    session = loginIntoBWAPPApplication(loginUrl)
+    xssPayloadFileName = os.path.dirname(__file__) + '/payloads/xss_payloads'
+    #errorBasedPayloadFileName =os.path.dirname(__file__) + '/payloads/error_based'
+    #timeBasedPayloadFileName=os.path.dirname(__file__) + '/payloads/time_based'
+    try:
+        #session = loginIntoBWAPPApplication(loginUrl)
+        session = requests.Session()
+        testAndGetSession(session,url)
+    except Exception as e:
+        print (e)
+        return
     print ("[+] Sending request for the URL web page")
-    targetParser = parseHtmlPage(url,session)
+    try:
+        targetParser = parseHtmlPage(url,session)
+        #print ("target parser: " + list(targetParser))
+    except Exception as e:
+        print (e)
+        return
     targetFormList = targetParser.formList
     print ("[+] Parsing completed")
 
     print ("[+] The following FORMS were found in the HTML document:")
     reportFormList = []
+    #print ("formList: " + str(targetParser.formList))
     for index,form in enumerate(targetParser.formList):
         dataValues = createDictionary(form)
         requestType = form.formType
@@ -48,65 +60,43 @@ def RUN(fileName):
 
     report["result"]["forms-tested"] = str(idList) 
     report["result"]["vulnerabilities"]=OrderedDict()
-    
-    for id in idList:
-        report["result"]["vulnerabilities"]["form-"+str(id)]=OrderedDict()
-        report["result"]["vulnerabilities"]["form-"+str(id)]["error-based"]=OrderedDict()
-        form = targetFormList[id]
-        dataValues = createDictionary(form,nonMaliciousValue)
-        filePath = attemptsFolder + filePrefix + str(id) + nonMaliciousResponseFileName
-        requestType = form.formType
-        print("[+] Sending non malicious request for FORM #" + str(id))
-        sendRequestAndSaveResponse(url,session,dataValues,requestType,filePath)
+    try:
+        for id in idList:
+            report["result"]["vulnerabilities"]["form-"+str(id)]=OrderedDict()
+            report["result"]["vulnerabilities"]["form-"+str(id)]["reflected"]=OrderedDict()
+            form = targetFormList[id]
+            dataValues = createDictionary(form,nonMaliciousValue)
+            filePath = attemptsFolder + filePrefix + str(id) + nonMaliciousResponseFileName
+            requestType = form.formType
+            print("[+] Sending non malicious request for FORM #" + str(id))
+            sendRequestAndSaveResponse(url,session,dataValues,requestType,filePath)
 
-        #Set our strategy for ERROR-BASED and test for every payload
-        detectedErrorBasedSQL = False
-        errorPayloadFile = open(errorBasedPayloadFileName,'r')
-        strategy = errorBasedSQLStrategy
-        print("[+] Testing form " + str(id) + " for error-based SQL injection detection")
-        for attempt,errorPayload in enumerate(errorPayloadFile.read().splitlines()): 
-            dataValues = createDictionary(form,errorPayload)
-            print("[+] Sending request for FORM #" + str(id) + ". PAYLOAD: " + errorPayload)
-            retVal = strategy(url,session,dataValues,requestType)
-            #If the first field of retVal is true, then there were vulnerabilities detected
-            if retVal[0]:
-                detectedErrorBasedSQL = True
-                print ("[!] Possible error-based vulnerability detected for form " 
-                        + str(id) + " using payload " + errorPayload)
-                report["result"]["vulnerabilities"]["form-"+str(id)]["error-based"]["payload "+errorPayload+" "]=retVal[1]
-                print ("Do you want to continue testing (y/n)?")
-                ans = input()
-                if (str.lower(ans) == "n"):
-                    break
-        errorPayloadFile.close()
+            #Set our strategy for Reflected and test for every payload
+            detectedXSSReflected = False
+            xssPayloadFile = open(xssPayloadFileName,'r')
+            strategy = xssReflectedStrategy
+            print("[+] Testing form " + str(id) + " for reflected XSS detection")
+            for attempt,xssPayload in enumerate(xssPayloadFile.read().splitlines()): 
+                dataValues = createDictionary(form,xssPayload)
+                print("[+] Sending request for FORM #" + str(id) + ". PAYLOAD: " + xssPayload)
+                retVal = strategy(url,session,dataValues,requestType)
+                #If the first field of retVal is true, then there were vulnerabilities detected
+                if retVal and retVal[0]:
+                    detectedXSSReflected = True
+                    print ("[!] Possible reflected xss vulnerability detected for form " 
+                            + str(id) + " using payload " + xssPayload)
 
-        #If an possible vulnerability was already detected, ask if the user wants to test for time-based
-        if detectedErrorBasedSQL:
-            print("[!] For FORM #" +str(id)+" there was an possible error-based SQL Injection " 
-                    + "vulnerability detected.")
-            print("Do you want to continue and test for time-based? (y/n)")
-            ans = input()
-            if (str.lower(ans) == "n"):
-                continue
-        
-        #Set our strategy for TIME-BASED and test for every payload
-        timeBasedPayloadFile = open(timeBasedPayloadFileName,'r')
-        report["result"]["vulnerabilities"]["form-"+str(id)]["time-based"]=OrderedDict()
-        strategy = timeBasedBlindSQLStrategy
-        print("[+] Testing form " + str(id) + " for time-based SQL injection detection")
-        for attempt,timePayload in enumerate(timeBasedPayloadFile.read().splitlines()): 
-            dataValues = createDictionary(form,timePayload)
-            print("[+] Sending request for FORM #" + str(id) + ". PAYLOAD: " + timePayload)
-            if strategy(url,session,dataValues,requestType):
-                print ("[!] Possible time-based vulnerability detected for form " 
-                        + str(id) + " using payload " + timePayload)
-                report["result"]["vulnerabilities"]["form-"+str(id)]["time-based"]["payload "+timePayload+" "]="success"
-                print ("Do you want to continue testing (y/n)?")
-                ans = input()
-                if (str.lower(ans) == "n"):
-                    break
-        timeBasedPayloadFile.close()
+                    report["result"]["vulnerabilities"]["form-"+str(id)]["reflected"]["payload "+xssPayload+" "]=retVal[1]
+                    print ("Do you want to continue testing (y/n)?")
+                    ans = input()
+                    if (str.lower(ans) == "n"):
+                        break
+            xssPayloadFile.close()
 
+    except Exception as e:
+        print ("[-] Too many request rejected. Host probably down.")
+        print (e)
+        return    
     closeResultsFile(resultsFile,report)
     return 0
         #payloadFile = open(payloadFileName,'r')
--- modules/xss/userlib.py
+++ modules/xss/userlib.py
@@ -5,12 +5,36 @@ import os
 from .parser import HTMLForm
 from .parser import HTMLTargetParser
 
+#Method for askink an user-informed cookie
+def getSessionWithCookie(session,url):
+    print("Inform the cookie")
+    cookie = input()
+    session.headers.update({'cookie':cookie})
+    return session
+
+#Testing for any redirects in the response
+def testAndGetSession(session,url):
+    x = session.get(url)
+    if x.history is not []:
+        print ("[!] It seems the URL is being redirected ("+str(x.history)+")")
+        print ("Redirection URL: " + x.url)
+        print ("If a login is required, then is recommended to inform a cookie for the session")
+        print ("Do you want to follow the redirect URL or inform a cookie? (f/c)")
+        ans = input()
+        if str.lower(ans) == "c":
+            session = getSessionWithCookie(session,url)
+        elif str.lower(ans) == "f":
+            print("[+] Following URL")
+        else:
+            print("[!] Invalid answer, following the redirect URL")
+    return session
+
 #Method for creating the results file for documenting all errors detected
 def createResultsFile(fileName,url,report):
     date = datetime.datetime.now()
     resultFileName = fileName+"_result"+".json"
     resultsFile = open(resultFileName,'w')
-    report["title"] = {"date":date.strftime("%Y-%m-%d %H:%M:%S"),"url":url}
+    report["title"] = "Test xss from date: " + str(date.strftime("%Y-%m-%d %H:%M:%S")) + " on the url: " + url
     return resultsFile
 
 #Dumping all the report fields into the JSON file
@@ -28,8 +52,7 @@ def loginIntoBWAPPApplication(loginUrl,timeout = 3):
         loginPayload = {'login':'bee','password':'bug','security_level':'0','form':'submit'}
         login = session.post(loginUrl, data=loginPayload,timeout = timeout)
     except requests.Timeout:
-        print ("[-] Login request timeout")
-        exit()
+        raise Exception("[-] Login request timeout") 
     return session
     
 #Method for parsing the web page and finding all HTML forms 
@@ -40,11 +63,9 @@ def parseHtmlPage(url,session):
         targetParser = HTMLTargetParser()
         targetParser.feed(r.text)
         if not targetParser.formList:
-            print ("[-] The given URL had no forms for targetting")
-            exit()
+            raise Exception("[-] The given URL had no forms for targetting")
     except requests.Timeout:
-        print ("[-] Request for page timeout")
-        exit()
+        raise Exception("[-] Request for page timeout")
     return targetParser
 
 #Method for creating the dictionary data structure according to the referred HTML Form
@@ -56,7 +77,7 @@ def createDictionary(form,value = None):
         if value is None:
             dataValues.update({dataField[1]:dataField[3]})
         else:
-            #The action field of a button will not be processed by SQL
+            #The action field of a button will not be processed by XSS
             if str.lower(dataField[1]) == 'action':
                 dataValues.update({dataField[1]:dataField[3]})
             else:
@@ -77,7 +98,7 @@ def sendRequestAndSaveResponse(url,session,dataValues,requestType,fileName,timeo
     f.write(s.text)
     f.close()
 
-def errorBasedSQLStrategy (url,session, dataValues,requestType):
+def xssReflectedStrategy (url,session, dataValues,requestType):
     timeout = 4
     result = False
     messageList=[]
@@ -89,46 +110,13 @@ def errorBasedSQLStrategy (url,session, dataValues,requestType):
     except requests.Timeout:
         print ("[-] Request timeout")
         return
-    with open(os.path.dirname(__file__) + "/sql_error_check","r") as checkFile:
-        checkLines = checkFile.readlines()
-        for line in checkLines:
-            if line.rstrip() in str.lower(s.text):
-                print ("[-] Detected \"" + line.rstrip() + "\" in the response, possible sql error-based vulnerability")
-                messageList.append(line.rstrip())
-                result = True
+    
+    payloadText = dataValues[list(dataValues.keys())[0]]
+
+    if str.lower(payloadText).rstrip() in str.lower(s.text):
+        #print ("[-] Detected \"" + payloadText + "\" in the response, possible XSS reflected vulnerability")
+        messageList.append(str(payloadText).rstrip())
+        result = True
     if not result:
         print ("[-] No errors detected")
     return result,messageList
-
-def timeBasedBlindSQLStrategy (url,session,dataValues,requestType):
-    #Defining the first timeout as 5 seconds
-    timeout = 5
-    retValue = False
-    try:
-        if (requestType == 'POST'):
-            s = session.post(url, data = dataValues,timeout = timeout)  
-        if (requestType == 'GET'):
-            s = session.get(url, params = dataValues,timeout = timeout)
-    except requests.Timeout:
-        #If the request timeouts, increase the timeout value to 10 seconds to ensure it is not a false positive
-        print ("[+] Possible time-based SQL injection vulnerability detected. Setting timeout for 10 seconds to reduce the chance of false-positive")
-        timeout = 10
-        print ("[+] Sending a new request to check if the server is OK.")
-        try:
-            if (requestType == 'POST'):
-                    s = session.post(url,timeout = timeout)  
-            if (requestType == 'GET'):
-                    s = session.get(url,timeout = timeout)
-            try:
-                print ("[+] Server is OK. Trying a new request to check ")
-                if (requestType == 'POST'):
-                    s = session.post(url, data = dataValues,timeout = timeout)  
-                if (requestType == 'GET'):
-                    s = session.get(url, params = dataValues,timeout = timeout)
-            except requests.Timeout:
-                print ("[+] Timeout reached, possible vulnerability detected. Yay!")
-                retValue = True
-                return retValue
-        except requests.Timeout:
-            print ("[-] Timeout reached, it seems that server crashed. Try again later.")
-            return retValue
